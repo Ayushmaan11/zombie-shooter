f33fe27eef586ea71250e16f203561d22b255233
Ayushmaan Pandey <maanayush478@gmail.com>
2026-07-08 22:36:17 +0530

    added grass textures

--- src/World.java
+++ src/World.java
@@ -1,3 +1,8 @@
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
 import java.awt.*;
 
 public class World {
@@ -6,12 +11,33 @@ public class World {
     private Color grassColor =  new Color(51, 102, 0);
     private int waterBorder = 300;
     private Color waterColor = new Color (0, 0, 51);
+    private BufferedImage grassTexture;
+    private final int TILE_SIZE = 32;
 
+    public World() {
+        try {
+            grassTexture = ImageIO.read(new File("assets/grass.png"));
+            System.out.println("Grass texture loaded");
+        } catch (IOException e) {}
+    }
     public void drawWorld(Graphics g, Camera camera){
+        int grassWidth = worldWidth-2*waterBorder;
+        int grassHeight = worldHeight-2*waterBorder;
+
+        int rows = grassHeight/TILE_SIZE;
+        int columns= grassWidth/TILE_SIZE;
         g.setColor(waterColor);
         g.fillRect(-camera.getXPos(), -camera.getYPos(), worldWidth, worldHeight);
-        g.setColor(grassColor);
-        g.fillRect(300-camera.getXPos(), 300-camera.getYPos(), worldWidth - 2*waterBorder, worldHeight - 2*waterBorder);
+        for(int row = 0; row <rows; row++){
+
+            for(int column = 0; column<columns; column++){
+                int titleWorldX = waterBorder + column*TILE_SIZE;
+                int titleWorldY = waterBorder + row*TILE_SIZE;
+                int screenX = titleWorldX - camera.getXPos();
+                int screenY = titleWorldY - camera.getYPos();
+                g.drawImage(grassTexture,screenX,screenY,TILE_SIZE,TILE_SIZE,null);
+            }
+        }
     }
 
     public int getWorldWidth() {
